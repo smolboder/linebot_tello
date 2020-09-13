509db806eea044969cf5a27be6c5a8fa6c4753b2
smolboder <70878802+smolboder@users.noreply.github.com>
2020-09-13 17:25:42 +0800

    Add files via upload

---
+++ tello_control.py
@@ -0,0 +1,171 @@
+import cv2 as cv
+from djitellopy import Tello
+from linebot.models import QuickReplyButton, MessageAction, TextSendMessage, QuickReply
+
+ifconnect = False
+me = Tello()
+
+
+def connect():
+    # connect to tello
+    me.connect()
+    me.for_back_velocity = 0
+    me.left_right_velocity = 0
+    me.up_down_velocity = 0
+    me.yaw_velocity = 0
+    me.speed = 0
+
+
+def show_battery():
+    """顯示電量"""
+    me.connect()
+    global ifconnect
+    if not ifconnect:
+        try:
+            connect()
+            ifconnect = True
+        except:
+            message = 'tello連接失敗'
+            return message
+    try:
+        battery = me.get_battery()
+    except:
+        message = 'tello連接失敗'
+        return message
+    return f"電量剩餘:{battery}%"
+
+
+def check_connectORnot(func):
+    def inner():
+        global ifconnect
+        if not ifconnect:
+            return '請先按1連接tello'
+        return func()
+    return inner
+
+
+@check_connectORnot
+def take_off():
+    me.takeoff()
+    return '成功收到起飛指令,準備起飛'
+
+
+@check_connectORnot
+def land():
+    global ifconnect
+    me.land()
+    ifconnect = False
+    return '成功收到降落指令,準備降落'
+
+
+@check_connectORnot
+def forward():
+    me.move_forward(30)
+    return '成功收到前進指令,準備前進'
+
+
+@check_connectORnot
+def back():
+    me.move_back(30)
+    return '成功收到後退指令,準備後退'
+
+
+@check_connectORnot
+def left():
+    me.move_left(30)
+    return '成功收到向左指令,準備向左'
+
+
+@check_connectORnot
+def right():
+    me.move_right(30)
+    return '成功收到向右指令,準備向右'
+
+
+@check_connectORnot
+def up():
+    me.move_up(50)
+    return '成功收到上升指令,準備上升'
+
+
+@check_connectORnot
+def down():
+    me.move_down(50)
+    return '成功收到下降指令,準備下降'
+
+
+@check_connectORnot
+def flipFoward():
+    me.flip_forward()
+    return '成功收到向前翻滾指令,準備向前翻滾'
+
+
+@check_connectORnot
+def flipBack():
+    me.flip_back()
+    return '成功收到向後翻滾指令,準備向後翻滾'
+
+
+@check_connectORnot
+def flipLeft():
+    me.flip_left()
+    return '成功收到向左指令,準備向左翻滾'
+
+
+@check_connectORnot
+def flipRight():
+    me.flip_right()
+    return '成功收到向右指令,準備向右翻滾'
+
+
+@check_connectORnot
+def rotate_clockwise_circle():
+    me.rotate_clockwise(360)
+    return '成功收到向右轉圈指令,準備右轉圈一圈'
+
+
+@check_connectORnot
+def rotate_clockwise_half_circle():
+    me.rotate_clockwise(180)
+    return '成功收到右轉指令,準備右轉180度'
+
+
+@check_connectORnot
+def rotate_unclockwise_circle():
+    me.rotate_clockwise(-360)
+    return '成功收到左轉指令,準備左轉一圈'
+
+
+@check_connectORnot
+def rotate_unclockwise_half_circle():
+    me.rotate_clockwise(-180)
+    return '成功收到向左轉圈指令,準備左轉180度'
+
+
+def Send_Text(event, selections):
+    """文字回應"""
+    F = quickReply(selections)
+    try:
+        message = TextSendMessage(
+            text='請選擇以下指令',
+            quick_reply=F,  # 快速選單
+        )
+    except:
+        message = TextSendMessage(text='發生錯誤')
+    return event, message
+
+
+def quickReply(selections: list):
+    """快速選單 使用在TextSendMessage裡的參數quick_reply"""
+    items = []  # 最多只能有13個
+    for select in selections:
+        items.append(
+            QuickReplyButton(
+                action=MessageAction(
+                    label=str(select),
+                    text='@' + str(select),
+                )
+            )
+        )
+    quick_reply = QuickReply(items=items)
+    return quick_reply
---
+++ views.py
@@ -0,0 +1,112 @@
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+from .module import tello_control
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                mtext = event.message.text
+                if mtext == '@顯示電量':
+                    message = tello_control.show_battery()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+                elif mtext == '@起飛降落':
+                    command = ['起飛','降落']
+                    event, message = tello_control.Send_Text(event,command)
+                    line_bot_api.reply_message(event.reply_token, message)
+
+                elif mtext == '@起飛':
+                    message = tello_control.take_off()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@降落':
+                    message = tello_control.land()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+                # quick select
+                elif mtext == '@移動':
+                    command = ['前進','後退','左平移','右平移']
+                    event, message = tello_control.Send_Text(event,command)
+                    line_bot_api.reply_message(event.reply_token, message)
+                elif mtext == '@旋轉':
+                    command = ['順時鐘旋轉360','順時鐘旋轉180','逆時針旋轉360','逆時針旋轉180']
+                    event, message = tello_control.Send_Text(event, command)
+                    line_bot_api.reply_message(event.reply_token, message)
+                elif mtext == '@翻滾':
+                    command = ['前滾翻','後滾翻','左滾翻','右滾翻']
+                    event, message = tello_control.Send_Text(event, command)
+                    line_bot_api.reply_message(event.reply_token, message)
+                elif mtext == '@升降':
+                    command = ['上升','下降']
+                    event, message = tello_control.Send_Text(event,command)
+                    line_bot_api.reply_message(event.reply_token, message)
+
+                elif mtext == '@前進':
+                    message = tello_control.forward()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@後退':
+                    message = tello_control.back()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@左平移':
+                    message = tello_control.left()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@右平移':
+                    message = tello_control.right()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+                elif mtext == '@前滾翻':
+                    message = tello_control.flipFoward()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@後滾翻':
+                    message = tello_control.flipBack()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@左滾翻':
+                    message = tello_control.flipLeft()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@右滾翻':
+                    message = tello_control.flipRight()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+                elif mtext == '@上升':
+                    message = tello_control.up()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@下降':
+                    message = tello_control.down()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+                elif mtext == '@順時鐘旋轉360':
+                    message = tello_control.rotate_clockwise_circle()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@逆時針旋轉360':
+                    message = tello_control.rotate_unclockwise_circle()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@順時鐘旋轉180':
+                    message = tello_control.rotate_clockwise_half_circle()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+                elif mtext == '@逆時針旋轉180':
+                    message = tello_control.rotate_unclockwise_half_circle()
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+
+            return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
